03c17d1aceefd513456a0ba4b5282ff19f8d3df5
amitbaghel7248-gif <amitbaghel7248@gmail.com>
2026-05-12 21:57:40 +0530

    Add files via upload

--- data_form_for_field.py
+++ data_form_for_field.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="Consumer Search", layout="centered")
 st.title("Genus Daily Billing Status")
 
 # data load
-df = pd.read_csv("data_for_web_from.csv")
+df = pd.read_csv(r"D:\billing_form\data_for_web_from.csv")
 
-search = st.text_input("**Search WFM_CONSUMER_NO / CCB_ACCT_ID / CCB_METER_NO**")
+search = st.text_input("**Search WFM_CONSUMER_NO/ WFM_METER_NO / CCB_ACCT_ID / CCB_METER_NO**")
 # st.markdown("**Search Consumer Number / RMS ACC / RMS MTR**")
 # search = st.text_input("")
 
@@ -16,8 +16,9 @@ if search:
 
     result = df[
         df["Consumer Number"].astype(str).str.contains(search) |
-        df["RMS ACC"].astype(str).str.contains(search) |
-        df["RMS MTR"].astype(str).str.contains(search)
+        df["Scan New Meter Serial No"].astype(str).str.contains(search) |
+        df["ACCT_ID"].astype(str).str.contains(search) |
+        df["METER_BADGE_NO"].astype(str).str.contains(search)
     ]
 
     if not result.empty:
@@ -25,36 +26,6 @@ if search:
         row = result.iloc[0]
 
         st.subheader("**Consumer Details**")
-
-        # st.text_input("**WFM_COSNUMER_NO**", row["Consumer Number"], disabled=True)
-        # st.text_input("**CCB_ACCT_ID**", int(row["RMS ACC"]), disabled=True)
-        # st.text_input("**CCB_METER_NO**", row["RMS MTR"], disabled=True)
-
-        # st.subheader("Consumer Details")
-
-        # st.markdown(f"<b style='color:green;'>WFM_COSNUMER_NO: {row['Consumer Number']}</b>", unsafe_allow_html=True)
-        # st.markdown(f"<b style='color:blue;'>CCB_ACCT_ID: {int(row['RMS ACC'])}</b>", unsafe_allow_html=True)
-        # st.markdown(f"<b style='color:red;'>CCB_METER_NO: {row['RMS MTR']}</b>", unsafe_allow_html=True)
-
-
-        # row = result.iloc[0]
-
-        # st.subheader("Consumer Details")
-
-        # st.markdown(
-        #     f"<b>WFM_COSNUMER_NO</b><br><span style='color:red; font-weight:bold;'>{row['Consumer Number']}</span>",
-        #     unsafe_allow_html=True
-        # )
-
-        # st.markdown(
-        #     f"<b>CCB_ACCT_ID</b><br><span style='color:red; font-weight:bold;'>{int(row['RMS ACC'])}</span>",
-        #     unsafe_allow_html=True
-        # )
-
-        # st.markdown(
-        #     f"<b>CCB_METER_NO</b><br><span style='color:red; font-weight:bold;'>{row['RMS MTR']}</span>",
-        #     unsafe_allow_html=True
-        # )
 
         st.markdown("""
         <style>
@@ -69,11 +40,72 @@ if search:
         st.markdown("**WFM_COSNUMER_NO**")
         st.text_input("WFM_COSNUMER_NO", row["Consumer Number"], disabled=True, label_visibility="collapsed")
 
-        st.markdown("**CCB_ACCT_ID**")
-        st.text_input("CCB_ACCT_ID", int(row["RMS ACC"]), disabled=True, label_visibility="collapsed")
+        st.markdown("**WFM_METER_NO**")
+        st.text_input("WFM_METER_NO", row["Scan New Meter Serial No"], disabled=True, label_visibility="collapsed")
 
-        st.markdown("**CCB_METER_NO**")
-        st.text_input("CCB_METER_NO", row["RMS MTR"], disabled=True, label_visibility="collapsed")
+        st.markdown("**CCB_ACCT_ID**")
+        st.text_input("CCB_ACCT_ID", row["ACCT_ID"], disabled=True, label_visibility="collapsed")
+
+        # st.markdown("**CCB_ACCT_ID**")
+        # st.text_input("CCB_ACCT_ID", int(row["ACCT_ID"]), disabled=True, label_visibility="collapsed")
+
+        st.markdown("**CCB_SERIAL_NO**")
+        st.text_input("CCB_SERIAL__NO", row["SERIAL_NBR"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**CCB_METER_BADGE_NO**")
+        st.text_input("CCB_METER_BADGE_NO", row["METER_BADGE_NO"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**CCB_LOAD_IN_KW**")
+        st.text_input("CCB_LOAD_IN_KW", row["LOAD_IN_KW"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**L1_STATUS**")
+        st.text_input("L1_STATUS", row["L1 Approval Status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**L1_STATUS_DATE**")
+        st.text_input("L1_STATUS_DATE", row["L1 Approval Date"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**L2_STATUS**")
+        st.text_input("L2_STATUS", row["L2 Approval Status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**L2_STATUS_DATE**")
+        st.text_input("L2_STATUS_DATE", row["L2 Approval Date"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**MCO_STATUS**")
+        st.text_input("MCO_STATUS", row["mco done or not"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**BILLING_PROFILE_STATUS**")
+        st.text_input("BILLING_PROFILE_STATUS", row["billing profile available or not"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**METER_STATUS**")
+        st.text_input("METER_STATUS", row["communication status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**READ_STATUS**")
+        st.text_input("READ_STATUS", row["read share status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**BILLED_STATUS**")
+        st.text_input("BILLED_STATUS", row["billing status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**MCO_ERROR_STATUS**")
+        st.text_input("MCO_ERROR_STATUS", row["mco error remark"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**METER_CHANGE**")
+        st.text_input("METER_CHANGE", row["O & M Meter"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**MC_L2_STATUS**")
+        st.text_input("MC_L2_STATUS", row["O & M L2 Status"], disabled=True, label_visibility="collapsed")
+
+        st.markdown("**MC_L2_STATUS_DATE**")
+        st.text_input("MC_L2_STATUS_DATE", row["O & M L2 Approval Date"], disabled=True, label_visibility="collapsed")
+
+        
+
+        
+
+
+
+
+
+
     
     
     else:
